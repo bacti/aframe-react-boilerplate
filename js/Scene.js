b83defc8078e67c0d6d5ba87e8ad289bd059c503
bacti <tranmhaii@gmail.com>
2018-05-02 16:34:14 +0700

    +

--- js/Scene.js
+++ js/Scene.js
@@ -2,80 +2,80 @@ import React, { Component } from 'react'
 import * as THREE from 'three'
 
 class Scene extends Component {
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
 
-    this.start = this.start.bind(this)
-    this.stop = this.stop.bind(this)
-    this.animate = this.animate.bind(this)
-  }
-
-  componentDidMount() {
-    const width = this.mount.clientWidth
-    const height = this.mount.clientHeight
-
-    const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      width / height,
-      0.1,
-      1000
-    )
-    const renderer = new THREE.WebGLRenderer({ antialias: true })
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({ color: '#433F81' })
-    const cube = new THREE.Mesh(geometry, material)
-
-    camera.position.z = 4
-    scene.add(cube)
-    renderer.setClearColor('#000000')
-    renderer.setSize(width, height)
-
-    this.scene = scene
-    this.camera = camera
-    this.renderer = renderer
-    this.material = material
-    this.cube = cube
-
-    this.mount.appendChild(this.renderer.domElement)
-    this.start()
-  }
-
-  componentWillUnmount() {
-    this.stop()
-    this.mount.removeChild(this.renderer.domElement)
-  }
-
-  start() {
-    if (!this.frameId) {
-      this.frameId = requestAnimationFrame(this.animate)
+        this.start = this.start.bind(this)
+        this.stop = this.stop.bind(this)
+        this.animate = this.animate.bind(this)
     }
-  }
 
-  stop() {
-    cancelAnimationFrame(this.frameId)
-  }
+    componentDidMount() {
+        const width = this.mount.clientWidth
+        const height = this.mount.clientHeight
 
-  animate() {
-    this.cube.rotation.x += 0.01
-    this.cube.rotation.y += 0.01
+        const scene = new THREE.Scene()
+        const camera = new THREE.PerspectiveCamera(
+            75,
+            width / height,
+            0.1,
+            1000
+        )
+        const renderer = new THREE.WebGLRenderer({ antialias: true })
+        const geometry = new THREE.BoxGeometry(1, 1, 1)
+        const material = new THREE.MeshBasicMaterial({ color: '#433F81' })
+        const cube = new THREE.Mesh(geometry, material)
 
-    this.renderScene()
-    this.frameId = window.requestAnimationFrame(this.animate)
-  }
+        camera.position.z = 4
+        scene.add(cube)
+        renderer.setClearColor('#000000')
+        renderer.setSize(width, height)
 
-  renderScene() {
-    this.renderer.render(this.scene, this.camera)
-  }
+        this.scene = scene
+        this.camera = camera
+        this.renderer = renderer
+        this.material = material
+        this.cube = cube
 
-  render() {
-    return (
-      <div
-        style={{ width: '400px', height: '400px' }}
-        ref={(mount) => { this.mount = mount }}
-      />
-    )
-  }
+        this.mount.appendChild(this.renderer.domElement)
+        this.start()
+    }
+
+    componentWillUnmount() {
+        this.stop()
+        this.mount.removeChild(this.renderer.domElement)
+    }
+
+    start() {
+        if (!this.frameId) {
+            this.frameId = requestAnimationFrame(this.animate)
+        }
+    }
+
+    stop() {
+        cancelAnimationFrame(this.frameId)
+    }
+
+    animate() {
+        this.cube.rotation.x += 0.01
+        this.cube.rotation.y += 0.01
+
+        this.renderScene()
+        this.frameId = window.requestAnimationFrame(this.animate)
+    }
+
+    renderScene() {
+        this.renderer.render(this.scene, this.camera)
+    }
+
+    render() {
+        return (
+            <div
+                style={{ width: '400px', height: '400px' }}
+                ref={(mount) => { this.mount = mount }}
+            />
+        )
+    }
 }
 
 export default Scene
